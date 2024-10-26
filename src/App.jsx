40578bfb1116dda450f8fcbde610c2dca4bf0e43
“Francisco <leanfran19@gmail.com>
2024-10-26 16:57:03 -0300

    nuevo commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { CartProvider } from './context/CartContext';
-import Cart from './components/cart/cart';
+import Cart from './components/Cart/Cart.jsx';
 
 
 function App() {
